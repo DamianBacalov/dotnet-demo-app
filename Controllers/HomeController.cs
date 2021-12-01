ce3f69bb6862afbe48c9aca5c98633a21c087e60
Damián Bacalov <damian.bacalov@wetcom.com>
2021-12-01 15:43:13 -0300

    Initial version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using dotnet_demo_app.Models;
+
+namespace dotnet_demo_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            this.config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = config.GetValue<string>("AppName");
+            ViewData["Company"] = config.GetValue<string>("COMPANY");
+            return View();
+        }
+    }
+}
